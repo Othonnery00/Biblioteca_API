ee7f93a906a404fbd96d782072188a1d2fbb35f4
Othon Nery <othonnery2@gmail.com>
2024-07-25 09:30:47 -0300

    Algumas alterações porém com erros que es~tão sendo corrigidos

--- src/main/java/br/com/Biblioteca/escritor/resource/EscritorResource.java
+++ src/main/java/br/com/Biblioteca/escritor/resource/EscritorResource.java
@@ -1,12 +1,54 @@
 package br.com.Biblioteca.escritor.resource;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.Biblioteca.escritor.domain.Escritor;
+import br.com.Biblioteca.escritor.service.EscritorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.util.ClassUtils.isPresent;
 
 @RestController
 @RequestMapping(value = "/escritor")
 public class EscritorResource {
 
+    @Autowired
+    private EscritorService escritorService;
+
+    @PostMapping
+    public ResponseEntity<Escritor> adicionarEscritor(@RequestBody Escritor escritorNovo){
+        Escritor escritorAdicionado = escritorService.adicionarEscritor(escritorNovo);
+        return new ResponseEntity<>(escritorAdicionado, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Escritor>> listarEscritor(){
+        List<Escritor> escritor = escritorService.listarEscritor();
+        return ResponseEntity.ok(escritor);
+    }
+
+    @GetMapping("/{idEscrittor}")
+    public ResponseEntity<Escritor> buscarEscritor(@PathVariable Integer idEscritor){
+        return escritorService.buscarEscritor(idEscritor)
+                .map(escritor -> new ResponseEntity<>(escritor, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+    }
+    @DeleteMapping("/{idPet}")
+    public ResponseEntity<Void> deletarEscritor(@PathVariable Integer idEscritor){
+        escritorService.deletarEscritor(idEscritor);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PutMapping
+    public ResponseEntity<Escritor> atualizarEscritor(@PathVariable Integer idEscritor, @RequestBody Escritor escritor){
+        if (EscritorService.buscarEscritor(idEscritor) isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        escritor.setId(idEscritor);
+        Escritor novoEscritor = escritorService.atualizarEscritor(escritor);
+        return new ResponseEntity<>(novoEscritor, HttpStatus.OK);
+    }
 }
--- src/main/java/br/com/Biblioteca/escritor/service/EscritorService.java
+++ src/main/java/br/com/Biblioteca/escritor/service/EscritorService.java
@@ -1,4 +1,35 @@
 package br.com.Biblioteca.escritor.service;
 
+import br.com.Biblioteca.escritor.domain.Escritor;
+import br.com.Biblioteca.escritor.repository.EscritorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EscritorService {
+    @Autowired
+    private static EscritorRepository escritorRepository;
+
+    public Escritor adicionarEscritor(Escritor novoEscritor) {
+        return escritorRepository.save(novoEscritor);
+    }
+
+    public List<Escritor> listarEscritor() {
+        return escritorRepository.findAll();
+    }
+
+    public Escritor atualizarEscritor(Escritor escritor) {
+        return escritorRepository.save(escritor);
+    }
+
+    public void deletarEscritor(Integer idEscritor){
+        escritorRepository.deleteAllById(idEscritor);
+    }
+
+    public static Optional<Escritor> buscarEscritor(Integer idEscritor){
+        return escritorRepository.findAllById(idEscritor);
+    }
 }
